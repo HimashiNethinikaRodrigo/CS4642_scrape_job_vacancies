0ede90aa6536d502c417b1310c73a31aef7f8331
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2018-06-19 18:21:20 +0530

    Scrapy for application.lk. Use first 200 pages.

--- spiders/application_spider.py
+++ spiders/application_spider.py
@@ -4,11 +4,52 @@ import scrapy
 class ApplicationSpider(scrapy.Spider):
     name = "applications"
 
-    start_urls = [
-        'https://www.applications.lk/job-vacancies/'
-    ]
-    for i in range(2, 10):
-        start_urls.append('https://www.applications.lk/job-vacancies/page/'+str(i))
+    def start_requests(self):
+
+        start_urls = [
+            'https://www.applications.lk/job-vacancies',
+        ]
+        for i in range(2, 201):
+            print(i)
+            start_urls.append('https://www.applications.lk/job-vacancies/page/'+str(i))
+
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        print(response)
+
+        print('RESPONSE --------------------------------------- : ', response.url)
+
+        if 'https://www.applications.lk/job-vacancies' == response.url or 'https://www.applications.lk/job-vacancies/page/' in response.url:
+            jobs = response.xpath('//article/div[@class="entry-content-wrapper"]/header[@class="entry-header"]/'
+                                  'h2[@class="entry-title"]/a/@href').extract()
+            for job in jobs:
+                yield scrapy.Request(job, callback=self.parse)
+
+        else:
+            title = response.xpath('//article/header[@class="entry-header"]/h1[@class="entry-title"]'
+                                   '/text()').extract_first()
+
+            posted_date = response.xpath('//article/header[@class="entry-header"]/div[@class="entry-meta"]'
+                                         '/span[@class="posted-on"]/a/time[@class="entry-date published updated"]/'
+                                         'text()').extract_first()
+            if posted_date == 'None':
+                posted_date = response.xpath('//article/header[@class="entry-header"]/div[@class="entry-meta"]'
+                                             '/span[@class="posted-on"]/a/time[@class="entry-date published"]/'
+                                             'text()').extract_first()
+
+            posted_time = response.xpath('//article/header[@class="entry-header"]/div[@class="entry-meta"]'
+                                         '/span[@class="posted-on"]/a/time/@datetime').extract_first()
+            author = response.xpath('//article/header[@class="entry-header"]/div[@class="entry-meta"]'
+                                    '/span[@class="byline"]/span[@class="author vcard"]'
+                                    '/a[@class="url fn n"]/text()').extract_first()
+            closing_date = response.xpath('//article/div[@class="entry-content-wrapper"]/div[@class="entry-content"]'
+                                          '/p/strong/span/text()').extract_first()
+
+            yield {
+                'title': title,
+                'posted_date': posted_date,
+                'posted_time': posted_time,
+                'author': author,
+                'closing_date': closing_date
+            }
